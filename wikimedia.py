bb21434ccd90d818cbfc71c5f684b24ddbb5a332
Ethan Chan <eton1234@outlook.com>
2023-05-01 19:30:53 -0500

    edited wikimedia.py

--- wikimedia.py
+++ wikimedia.py
@@ -3,43 +3,66 @@ import requests
 import json
 import wikipediaapi
 import wikipedia
+import re, string
 
-#outputs an array where each element is a dictionary with the format, {content: text, url: "https://..."}
-def search_wikipedia(query):
-  output = []
-  #first try to search for the page directly
-  try:
-    page = returnPageContents(query)
-    if page:
-      # print("successfully found topic")
-      output.append({"content": page.content,"url": str(page.url)})
-      return output
-  except:
-     pass
-#otherwise search for related search topics
-  # print("searching for corresponding wikipage")
-  search_results = wikipedia.search(query)
-  # print(search_results)
-  for result in search_results:
-    page = returnPageContents(result)
-    if page:
-      output.append({"content": page.content,"url": str(page.url)})
-  return output
-  
+
+class Source:
+  def __init__(self, url, text):
+    self.url = url
+    self.text = text
+
+class Wikipedia:
+    def __init__(self,query):
+      self.sources = []
+      #loading sources
+      try:
+          #first try to search for the page directly
+          s = wikipedia.page(str(query))
+          if s:
+            self.sources.append(src)
+          self.getPageContents(query)
+          if s:
+            # print("successfully found topic")
+            src = Source(s.url, s.content)
+            self.sources.append(src)
+      except:
+        print("excepted first try. now searching for multiple pages")
+        search_results = wikipedia.search(query)
+        # print(search_results)
+        for result in search_results:
+          try:
+            s= wikipedia.page(str(result))
+            if s:
+              src = Source(s.url, s.content)
+              self.sources.append(src)
+          except:
+            pass
+    def getTextSegs(self,leng=4000):
+      segs = []
+      for src in self.sources:
+        #split up text into segments with leng number of characters
+        text = src.text
+        # use regex to replace this pattern \n\n\n==== Go ====\n
+        # text = re.sub(r'[^A-Za-z0-9 ]+', '', text)
+        text = re.sub(' +', ' ', text)
+        text = clean_string(text)
+        while len(text) > leng:
+          segs.append(text[:leng])
+          text = text[leng:]
+        segs.append(text)
+      return segs
 #helper function
-def returnPageContents(page):
-  try:
-    s = wikipedia.page(str(page))
-    if s:
-      return s
-    else:
-      return None
-  except:
-    return None
 
 
 
-
+def clean_string(input_string):
+    cleaned_string = re.sub(r'==+.*?==+', '', input_string)  # Remove section headings
+    cleaned_string = re.sub(r'{{.*?}}', '', cleaned_string)  # Remove templates
+    cleaned_string = re.sub(r'\[\[.*?\]\]', '', cleaned_string)  # Remove internal links
+    cleaned_string = re.sub(r'<.*?>', '', cleaned_string)  # Remove HTML tags
+    cleaned_string = re.sub(r'[^a-zA-Z0-9()\s-]+', '', cleaned_string)  # Remove non-word characters except parentheses, dashes, and spaces
+    cleaned_string = re.sub(r'\s+', ' ', cleaned_string)  # Replace multiple spaces with a single space
+    return cleaned_string.strip()
 
 ##############################################################################
 ''' Deprecated --------------------------------------------------'''
