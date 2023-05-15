1276c3ff2d29a1ecfd9114d5a977864150f838ca
Ethan Chan <eton1234@outlook.com>
2023-05-15 00:17:18 -0500

    GPT output -> facts -> google search api

---
+++ V2/chatgpt.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Given a prompt, getGptText returns ChatGPT response
+def getGptText(prompt):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=50,
+        temperature=0
+    )
+
+    # Print the generated text from the response
+    generated_text = response['choices'][0]['text']
+    return(generated_text)
---
+++ V2/main.py
@@ -0,0 +1,34 @@
+#Main file
+import argparse
+import chatgpt
+from goo import *
+#Take initial prompt
+def getPrompt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output', help='output file')
+    args = parser.parse_args()
+    if args.output:
+        with open(args.output, 'w') as f:
+            user_input = input('Enter a prompt to chatgpt: ')
+            f.write(user_input)
+    else:
+        user_input = input('Enter a prompt for chatgpt: ')
+        print('You entered:', user_input)
+    return user_input
+
+
+
+user_input = getPrompt()
+#make call to chatgpt using chatgpt.py and get response
+response = chatgpt.getGptText(user_input)
+print(response)
+topictext = chatgpt.getGptText("reformat as a list of context independent searchable questions for verification of the validity of all facts stated. No nonidentificable pronouns. Here is the response: " + response)
+topictext = topictext.split("\n")
+print(topictext)
+for topic in topictext:
+    print("###########processing topic##########")
+    print("topic: " + topic)
+    if len(topic) > 1:
+        print_snips(topic)
+#identify sources 
+
